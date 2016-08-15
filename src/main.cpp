4ca11fd1da3a57f958f9c67d7cc847025d4f603f
Pavel Khlebovich <pas.nteg@gmail.com>
2016-08-15 20:36:12 +0300

    Support custom QSS styles

--- src/main.cpp
+++ src/main.cpp
@@ -135,6 +135,19 @@ int main(int argc, char *argv[])
         workdir = QDir::currentPath();
     app->setWorkingDirectory(workdir);
 
+    const QSettings settings;
+    const QFileInfo customStyle = QFileInfo(
+        QFileInfo(settings.fileName()).canonicalPath() +
+        "/style.qss"
+    );
+    if (customStyle.isFile() && customStyle.isReadable())
+    {
+        QFile style(customStyle.canonicalFilePath());
+        style.open(QFile::ReadOnly);
+        QString styleString = QLatin1String(style.readAll());
+        app->setStyleSheet(styleString);
+    }
+
     // icons
     /* setup our custom icon theme if there is no system theme (OS X, Windows) */
     if (QIcon::themeName().isEmpty())
